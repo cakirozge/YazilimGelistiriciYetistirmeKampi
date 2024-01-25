5b1ee0baa0d0014adbb299cf161bf7479fd3c638
Ozge Cakir <caakir.ozge@gmail.com>
2024-01-25 23:15:13 +0300

    Yazılım Geliştirici Yetiştirme Kampı (C# + ANGULAR) 4.Gun

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    //Urun ile ilgili operasyonları içerir.
+    //Arama - ekleme - silme - filtreleme
+    //void emir kipinde çağrı yapar.
+    public class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + "eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "güncellendi.");
+        }
+    }
+}
